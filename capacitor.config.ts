555a693c5377f1da210979871e5f378277a02b45
John Corser <xss@amazon.com>
2025-01-06 19:19:37 -0500

    chore: add apple-app-site-association

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,6 +3,10 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.johncorser.countdowns",
   appName: "jpc.countdowns",
+  server: {
+    hostname: "countdowns.jpc.io",
+    androidScheme: "https",
+  },
   webDir: "dist",
   ios: {
     contentInset: "always",
