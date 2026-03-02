a56fc284c33cee7528728d6448b865dda0f0de9a
John Corser <johnpc@umich.edu>
2026-03-02 00:32:54 -0500

    Set Cognito refresh token to max (10 years)

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -5,7 +5,12 @@ import { data } from "./data/resource";
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+// Extend refresh token validity to 10 years (max allowed)
+const { cfnUserPoolClient } = backend.auth.resources.cfnResources;
+cfnUserPoolClient.refreshTokenValidity = 3650;
+cfnUserPoolClient.tokenValidityUnits = { refreshToken: "days" };
